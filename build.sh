0bc2fbdcaf81e3f040f8b61958e2e95e425b38df
Ernest Micklei <ernest.micklei@gmail.com>
2015-01-30 10:50:13 +0100

    fix call run
    Change-Id: I642d930730031e20deb1463b4c1956b9d11a0643

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -rf target \
 && mkdir target \
 && docker build -t typhoon-builder . \
-&& docker run --rm -v -e VERSION=$GIT_COMMIT `pwd`/target:/target -t $(docker images -q | head -1) \
+&& docker run --rm -e VERSION=$GIT_COMMIT -v `pwd`/target:/target -t $(docker images -q | head -1) \
 && ls -l target
