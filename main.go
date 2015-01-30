02e5accb4024da90db7b64b397b5284c85e2f712
Ernest Micklei <ernest.micklei@gmail.com>
2015-01-30 15:51:17 +0100

    add fetch, change args
    Change-Id: Ie8d6ea27983237ccaf45f63bf1f5035390fe7a08

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// go run *.go archive --artifact=main.go --group=com.ubanita --version=1.0-SNAPSHOT --force=true
+// go run *.go archive --artifact=main.go --group=com.ubanita --version=1.0-SNAPSHOT
+// go run *.go fetch --artifact=main.go --group=com.ubanita --version=1.0-SNAPSHOT hier
 
 var VERSION string = "dev"
 var BUILDDATE string = "now"
@@ -22,38 +23,57 @@ var RootCmd = &cobra.Command{
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
-type archiveCmd struct {
+type artifactCmd struct {
 	*cobra.Command
-	artifact  string
-	group     string
-	version   string
+	artifact string
+	group    string
+	version  string
+}
+
+type archiveCmd struct {
+	*artifactCmd
 	overwrite bool
 }
 
-func newArchiveCmd() *cobra.Command {
-	cmd := new(archiveCmd)
-	cmd.Command = &cobra.Command{
-		Use:   "archive",
-		Short: "copy an artifact to the typhoon repository",
-		Run:   cmd.doArchive,
-	}
+func newArtifactCmd(cobraCmd *cobra.Command) *artifactCmd {
+	cmd := new(artifactCmd)
+	cmd.Command = cobraCmd
 	cmd.PersistentFlags().StringVar(&cmd.artifact, "artifact", ".", "file location of artifact to copy")
 	cmd.PersistentFlags().StringVar(&cmd.group, "group", ".", "store the articat under this group")
 	cmd.PersistentFlags().StringVar(&cmd.version, "version", ".", "store the articat under this version")
-	cmd.PersistentFlags().BoolVar(&cmd.overwrite, "force", false, "force overwrite if version exists")
+	return cmd
+}
+
+func newArchiveCmd() *cobra.Command {
+	cmd := newArtifactCmd(&cobra.Command{
+		Use:   "archive [artifact]",
+		Short: "copy an artifact to the typhoon repository",
+	})
+	archiveCmd := new(archiveCmd)
+	archiveCmd.artifactCmd = cmd
+	archiveCmd.PersistentFlags().BoolVar(&archiveCmd.overwrite, "force", false, "force overwrite if version exists")
+	cmd.Command.Run = archiveCmd.doArchive
 	return cmd.Command
 }
 
-func (a *archiveCmd) doArchive(cmd *cobra.Command, args []string) {
+func getRepo() string {
 	repo := os.Getenv("TYPHOON_REPO")
 	if len(repo) == 0 {
 		log.Fatal("missing TYPHOON_REPO environment setting")
 	}
+	return repo
+}
+
+func (a *archiveCmd) doArchive(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		log.Fatalf("missing artifact")
+	}
+	a.artifact = args[len(args)-1]
 	g := path.Join(strings.Split(a.group, ".")...)
 	regular := path.Base(path.Clean(a.artifact))
-	p := path.Join(repo, g, regular, a.version)
+	p := path.Join(getRepo(), g, regular, a.version)
 
-	log.Printf("copying %s into folder %s", regular, p)
+	log.Printf("copying %s into folder %s\n", regular, p)
 	if err := os.MkdirAll(p, os.ModePerm); err != nil {
 		log.Fatalf("unable to create dirs: %s cause: %v", p, err)
 	}
@@ -72,9 +92,41 @@ func (a *archiveCmd) doArchive(cmd *cobra.Command, args []string) {
 	}
 }
 
+type fetchCmd struct {
+	*artifactCmd
+	destination string
+}
+
+func newFetchCmd() *cobra.Command {
+	cmd := newArtifactCmd(&cobra.Command{
+		Use:   "fetch [destination]",
+		Short: "copy an artifact from the typhoon repository to [destination]",
+	})
+	fetch := new(fetchCmd)
+	fetch.artifactCmd = cmd
+	cmd.Command.Run = fetch.doFetch
+	return cmd.Command
+}
+
+func (f *fetchCmd) doFetch(cmd *cobra.Command, args []string) {
+	g := path.Join(strings.Split(f.group, ".")...)
+	src := path.Join(getRepo(), g, f.artifact, f.version, f.artifact)
+	if len(args) == 0 {
+		log.Fatalf("missing destination")
+	}
+	destination := args[len(args)-1]
+	log.Printf("copying %s to %s\n", src, destination)
+	if err := Cp(destination, src); err != nil {
+		log.Fatalf("unable to copy artifact: %s to: %s cause:%v", src, f.destination, err)
+	}
+}
+
 func main() {
-	log.Println("_/^\\_/^\\_/^\\_ typhoon - the artifact tool [commit=", VERSION, "build=", BUILDDATE, "] _/^\\_/^\\_/^\\_")
+	log.Println("_/^\\_")
+	log.Println(" | | typhoon - the artifact tool [commit=", VERSION, "build=", BUILDDATE, "]")
+	log.Println("-\\_/-")
 	RootCmd.AddCommand(newArchiveCmd())
+	RootCmd.AddCommand(newFetchCmd())
 	RootCmd.Execute()
 }
 
