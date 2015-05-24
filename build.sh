07404417fa7a4a275b916fcc3bb6cc97137f87f1
Ernest Micklei <emicklei@Ernests-iMac.local>
2015-05-24 20:40:57 +0200

    retry dood build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ rm -rf target \
 && mkdir target \
 && docker build --no-cache=true -t artreyu-builder . \
 && echo `pwd` \
-&& docker run --rm -e VERSION=$GIT_COMMIT -v `pwd`/target:/target -t $(docker images -q | head -1) \
+&& docker run --rm -e VERSION=$GIT_COMMIT -v target:/target -t $(docker images -q | head -1) \
 && ls -l target
