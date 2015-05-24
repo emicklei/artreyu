a0de1ebf9b4f9b16848b9b38b08928f9f6bd9aad
Ernest Micklei <emicklei@Ernests-iMac.local>
2015-05-24 16:57:33 +0200

    log build loc

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 rm -rf target \
 && mkdir target \
 && docker build --no-cache=true -t artreyu-builder . \
+&& echo `pwd` \
 && docker run --rm -e VERSION=$GIT_COMMIT -v `pwd`/target:/target -t $(docker images -q | head -1) \
 && ls -l target
